e4ab75ad43b75caa812c959353c2e87584f3bbbe
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-06-20 16:59:23 +0530

    adding details

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,3 @@
 compartment_id = "ocid1.compartment.oc1..aaaaaaaah6ibn4qjy6chh7ilzha53oeeacmrmghdh5ziqhzn2xtgubhxolga"
+vcn_cidr_block = "11.0.0.0/16"
+vcn_display_name = "appdev-workshop-vcn"
--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@ variable "compartment_id" {
   description = "The compartment id where to create all resources."
   type        = string
 }
+
+variable "vcn_cidr_block" {
+  description = "The compartment id where to create all resources."
+  type        = string
+}
+
+
+variable "vcn_display_name" {
+  description = "The compartment id where to create all resources."
+  type        = string
+}
